140b81d93c855852ce64d958822f758d7ffea63d
zemmediamatiker <stefan.eckstein@zem.ch>
2016-02-16 15:47:33 +0100

    16.02.2016
    Commit für den zweiten IPA-Tag.
    
    Heute habe ich gemäss meiner Zielsetzung für den heutigen Tag den
    Bereich zum Thema „Wie werde ich MediamatikerIn“ umgesetzt. Innerhalb
    der Projektdateien habe ich angefangen die einzelnen Teile mit
    Kommentaren verständlich zu erklären, so dass später auch jemand
    anderes an diesem Projekt arbeiten kann.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-jQuery(window).load(function() {
+/*jQuery(window).load(function() {
 	jQuery("#preloader").delay(100).fadeOut("slow");
 })
+*/
 
+
+
+jQuery(document).ready(function($) {
+
+    $(document).ready(function (){
+            $(".scrolldown-arrow").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#mdwie").offset().top
+                }, 1000);
+            });
+        });
+
+				
+});
