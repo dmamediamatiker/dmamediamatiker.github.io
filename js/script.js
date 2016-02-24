7df36fa241d10ef55057ddc373f3986471377a02
zemmediamatiker <stefan.eckstein@zem.ch>
2016-02-24 16:41:43 +0100

    24.02.16
    Commit Tag 6.
    Heute habe ich nicht viel an der Projektwebseite gearbeitet, sondern
    mich auf die Dokumentation konzentriert.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
+/*  
+    Die folgende Funktion ist für den Loadingscreen. Wenn die Seite vollständig geladen ist, wird dieser    nach einer Sekunde ausgeblendet. Mit den Einstellungen in der weiteren Funktion werdenverschiedenen Elemente Klassen entfernt und hinzugefügt, dass sie Seite richtig funktioniert und so erscheint, wie es vorgesehen ist.
+*/
 jQuery(window).load(function() {
 	jQuery("#mdloader").delay(1000).fadeOut("slow", function(){
                                             
-        $("body").removeClass("noscroll");
-        $("#mdhome-skyline").removeClass("vhidden"); 
-        $("#mdhome-skyline").addClass("fadeIn");
-        $("#mdhometext").removeClass("vhidden"); 
-        $("#mdhometext").addClass("fadeInUp"); 
-        $(".scrollhint").addClass("bounce");
-        $("#skyline").addClass("animatedskyline");
+        $("body").removeClass("noscroll");              // Entfernt die Klasse, die verhindert dass man im Loadinscreen scrollen kann.
+        $("#mdhome-skyline").removeClass("vhidden");    // Blendet das Objekt ein
+        $("#mdhome-skyline").addClass("fadeIn");        // Fügt die Animate.css-Klasse für die Animation hinzu
+        $("#mdhometext").removeClass("vhidden");        // Blendet das Objekt ein
+        $("#mdhometext").addClass("fadeInUp");          // Fügt die Animate.css-Klasse für die Animation ein
+        $(".scrollhint").addClass("bounce");            // Fügt dem Element einen Bounce-Effekt hinzu
+        $("#skyline").addClass("animatedskyline");      // Fügt dem Element die Klasse für die SVG-Animation zu, damit die Animation erst startet, wenn der Nutzer die Webseite und nicht mehr den Loadingscreen sieht.
     });
     
 });
 
+/*
+    In der folgenden Funktion werden alle jQuery-Befehle ausgeführt, wenn das Dokument bereit ist.
+*/
 jQuery(document).ready(function($) {
     
 
